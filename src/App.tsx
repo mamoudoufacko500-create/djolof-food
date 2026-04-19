e520e575ea605d4e1fccab8c5f866b8d4b8d2935
unknown <mamoudoufacko500@gamil.com>
2026-04-19 17:16:34 -0400

    feat: initial commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { BrowserRouter } from "react-router-dom"
+import { Toaster } from "sonner"
+
+
+function App() {
+ 
+
+  return (
+    <BrowserRouter>
+    <Toaster />
+    </BrowserRouter>
+  )
+}
+
+export default App
